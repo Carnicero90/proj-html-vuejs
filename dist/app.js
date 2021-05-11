648082ba720c43d17a123696eff76ed67f54a9ea
Filippo Montani <filippomontani@Wendigo.homenet.telecomitalia.it>
2021-05-11 10:40:59 +0200

    primo commit

---
+++ dist/app.js
@@ -0,0 +1,24 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/app.scss":
+/*!**********************!*\
+  !*** ./src/app.scss ***!
+  \**********************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Can't find stylesheet to import.\n  ╷\n1 │ @import 'partials/basics'\n  │         ^^^^^^^^^^^^^^^^^\n  ╵\n  src/app.scss 1:9  root stylesheet\n    at processResult (/Users/filippomontani/bool/esercitazione/node_modules/webpack/lib/NormalModule.js:676:19)\n    at /Users/filippomontani/bool/esercitazione/node_modules/webpack/lib/NormalModule.js:778:5\n    at /Users/filippomontani/bool/esercitazione/node_modules/loader-runner/lib/LoaderRunner.js:399:11\n    at /Users/filippomontani/bool/esercitazione/node_modules/loader-runner/lib/LoaderRunner.js:251:18\n    at context.callback (/Users/filippomontani/bool/esercitazione/node_modules/loader-runner/lib/LoaderRunner.js:124:13)\n    at /Users/filippomontani/bool/esercitazione/node_modules/sass-loader/dist/index.js:54:7\n    at Function.call$2 (/Users/filippomontani/bool/esercitazione/node_modules/sass/sass.dart.js:92612:16)\n    at _render_closure1.call$2 (/Users/filippomontani/bool/esercitazione/node_modules/sass/sass.dart.js:81115:12)\n    at _RootZone.runBinary$3$3 (/Users/filippomontani/bool/esercitazione/node_modules/sass/sass.dart.js:27260:18)\n    at _FutureListener.handleError$1 (/Users/filippomontani/bool/esercitazione/node_modules/sass/sass.dart.js:25816:19)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/app.scss"]();
+/******/ 	
+/******/ })()
+;
