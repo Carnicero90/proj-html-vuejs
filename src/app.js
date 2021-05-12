4720255a4179c1e7ae63c8ac096492ae7169e498
Filippo Montani <filippomontani@Wendigo.homenet.telecomitalia.it>
2021-05-12 12:05:46 +0200

    miglioramenti grafici

---
+++ src/app.js
@@ -0,0 +1,10 @@
+socials = [
+    {'twitter': '#'},
+    {'facebook': '#'},
+    {'instagram': '#'},
+    {'linkedin': '#'}
+];
+headerUl = ['Home', 'Pages', 'Courses', 'Features', 'Blog', 'Shop'];
+
+footerUls = {'Explore': [],
+            'Information': []};
