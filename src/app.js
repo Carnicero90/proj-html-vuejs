9e2cc3a3dd80b628bd5cbbdc6ab9ca3d3d4fcf2c
Filippo Montani <filippomontani@Wendigo.local>
2021-05-13 09:36:12 +0200

    abbozzato header

--- src/app.js
+++ src/app.js
@@ -1,10 +1,55 @@
-socials = [
-    {'twitter': '#'},
-    {'facebook': '#'},
-    {'instagram': '#'},
-    {'linkedin': '#'}
-];
-headerUl = ['Home', 'Pages', 'Courses', 'Features', 'Blog', 'Shop'];
+var app = new Vue({
+    el: '#root',
+    data: {
+        socials: {
+            'twitter': {
+                link: 'https://twitter.com/maxcoach',
+                awesome: 'fa-twitter'
+            },
+            'facebook': {
+                link: 'https://facebook.com/maxcoach',
+                awesome: 'fa-facebook'
+            },
+            'instagram': {
+                    link: 'https://instagram.com/maxcoach',
+                     awesome: 'fa-instagram' },
+            'linkedin': {
+                link: 'https://linkedin.com/maxcoach',
+                awesome: 'fa-linkedin'}
+            },
+        header: {
+            ul: [
+                'Home',
+                'Pages',
+                'Courses',
+                'Features',
+                'Blog',
+                'Shop'
+            ],
+            socialsList: ['twitter', 'facebook', 'instagram', 'linkedin'],
+            socialsStyle: 'fab'
 
-footerUls = {'Explore': [],
-            'Information': []};
+            },
+            footer: {
+                socialsList: ['facebook', 'twitter', 'instagram', 'linkedin'],
+                socialsStyle: 'fas'
+            }
+        }
+
+
+
+
+    })
+
+socials = [
+    { 'twitter': '#' },
+    { 'facebook': '#' },
+    { 'instagram': '#' },
+    { 'linkedin': '#' }
+];
+headerUl = [];
+
+footerUls = {
+    'Explore': [],
+    'Information': []
+};
